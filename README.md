6296d57a8c29f7af336fa70369750401d6e2ab25
Ethan <meshethan@gmail.com>
2022-02-22 18:00:09 +0800

    ✨ feat(adv): demo

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # learn-go
+
+
+# 学习路径
+* [Go 语言之旅](https://tour.go-zh.org/welcome/1)
+* [Go 语言圣经](https://github.com/golang-china/gopl-zh)
+* [Go 高级编程](https://github.com/chai2010/advanced-go-programming-book)
---
+++ gopl/dup1.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		line := input.Text()
+		if line == "exit" {
+			break
+		}
+		counts[line]++
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
